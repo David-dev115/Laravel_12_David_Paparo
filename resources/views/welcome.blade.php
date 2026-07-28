604ab608dd8324ef68e6b71f98a1da674e6fa432
David Paparo <d.paparotiscali.it>
2026-07-28 15:29:57 +0200

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body>
+
+    <h1>titolo</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad rerum nulla recusandae debitis obcaecati ea! Quis deleniti sunt possimus culpa, quae odio, debitis assumenda non, vel eveniet vitae expedita accusamus?</p>
+    
+</body>
+</html>
